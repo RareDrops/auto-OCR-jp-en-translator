78fa27424c5656c75aa6c3d50799d082278f1a01
RareDrops <54132759+RareDrops@users.noreply.github.com>
2022-01-24 22:19:59 +1300

    Update translatordeepl.py

--- functions/translatordeepl.py
+++ functions/translatordeepl.py
@@ -3,19 +3,23 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
+import requests
   
 # url of the page to scrape
 url = "https://www.deepl.com/en/translator#ja/en/DeepL%E3%81%B8%E3%82%88%E3%81%86%E3%81%93%E3%81%9D"
   
 # initiating the webdriver.
-driver = webdriver.Chrome('./chromedriver') 
+driver = webdriver.Chrome('./chromedriver')
 driver.get(url) 
   
 # this is just to ensure that the page is loaded
 #time.sleep(5) 
 
-#html = driver.page_source 
+html = driver.page_source 
 # renders the JS code and stores all
 # of the information in static HTML code.
 
@@ -23,9 +27,10 @@ driver.get(url)
 #soup = BeautifulSoup(html, "html.parser")
 class_name = driver.find_element(By.CLASS_NAME, "lmt__textarea")
 def deepl_translator(text):
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
     class_name.clear()
     class_name.send_keys(str(text)) # to be replaced by a variable
+    time.sleep(3)
+    html = driver.page_source.encode('utf-8')
+    soup = BeautifulSoup(html, "html.parser")
     all_divs = soup.find('div', {'id' : 'target-dummydiv'}) # this gets the translated text, continue with this tmr
-    print(all_divs)
+    return all_divs.get_text()
