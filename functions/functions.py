a1196d86dae750b18571509ae4b7a97dfc73643c
RareDrops <54132759+RareDrops@users.noreply.github.com>
2022-01-25 19:06:48 +1300

    Update functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -1,20 +1,6 @@
-from PIL import ImageGrab, ImageOps, Image
-#import pytesseract
+from PIL import ImageGrab, ImageOps
 from functions.translatordeepl import deepl_translator
 import easyocr
-import asyncio
-
-#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-#def image_reader(coordinates):
-#    im = ImageGrab.grab(bbox=coordinates)
-#    im = ImageOps.grayscale(im)
-#    im = ImageOps.invert(im.convert("L"))
-#    im.save("./Assets/screenshot_area.png")
-#    result = pytesseract.image_to_string(Image.open('./Assets/screenshot_area.png'), lang='jpn')
-#    result = "".join(result.split()) #removes whitespace
-#    deepl_translator(result)
-#    return image_reader
 
 previous=None
 previous_translated=None
